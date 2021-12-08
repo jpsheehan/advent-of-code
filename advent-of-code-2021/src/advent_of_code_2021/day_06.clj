38143f1f2b3c9376505e11f50ee8548818f1005c
Jesse Sheehan <jesse.sheehan@phocassoftware.com>
2021-12-08 16:34:15 +1300

    Added tests for day 6 part 2. Added memoization.

--- advent-of-code-2021/src/advent_of_code_2021/day_06.clj
+++ advent-of-code-2021/src/advent_of_code_2021/day_06.clj
@@ -1,83 +1,45 @@
 (ns advent-of-code-2021.day-06
   (:require [clojure.string :as string]))
 
-(def fish-reproductive-period 7)
-(def new-fish-extra-days 2)
-
-(defn next-state
-  "Returns a list containing the next state(s) for the fish."
-  [fish]
-  (if (zero? fish)
-    (list (dec fish-reproductive-period) (+ (dec fish-reproductive-period) new-fish-extra-days))
-    (list (dec fish))))
-
-;(def next-state-memo (memoize next-state))
-
-(defn simulation-step
-  "Runs a single step of the simulation."
-  [fishes]
-  (->> fishes
-       (map next-state)
-       (flatten)))
+; Memoization saves the day here. To run the simulation for part one, it takes:
+; - 68 ms without memoization
+; - 7 ms with memoization
+;
+; That's one order of magnitude difference and the delta will only increase as the number of days increases.
+(def fish-memoized
+  (memoize
+    (fn
+      [days-until-split days-until-end starting-num]
+      (loop [s days-until-split
+             e days-until-end
+             n starting-num]
+        (if (zero? e)
+          n                                                 ; the base case
+          (let [next-s (if (zero? s) 6 (dec s))             ; decrement the number of days until splitting, unless we should wrap around to 6
+                next-e (dec e)
+                next-n (if (zero? s) (+ (inc n) (fish-memoized 8 next-e 0)) n) ; recursively call this fn if we are supposed to be splitting
+                ]
+            (recur next-s next-e next-n)))))))
 
 (defn simulation-run
-  "Runs the simulation for n days."
+  "Runs the simulation for n days. Returns the number of fish."
   [initial-state n]
-  (loop [state initial-state
-         n n]
-    (if (zero? n)
-      state
-      (recur (simulation-step state)
-             (dec n)))))
-
-;(defn get-partition-size
-;  "Returns an appropriate partitions size."
-;  [number-fish thread-count]
-;  (if (< number-fish thread-count)
-;    1
-;    (long (Math/ceil (/ number-fish thread-count)))))
-
-;(defn mt-simulation-run
-;  "Runs the simulation for n days in t threads."
-;  [initial-state n t]
-;  (let [partition-size (get-partition-size (count initial-state) t)
-;        partitioned-state (partition-all partition-size initial-state)
-;        futures (map (fn [fishes] (future (simulation-run fishes n))) partitioned-state)
-;        _ (map #(deref % 100 :timeout) futures)
-;        completed-futures (map #(deref %) futures)]
-;    (flatten completed-futures)))
+  (->> (map (fn [num-days-until-split] (fish-memoized num-days-until-split n 1)) initial-state)
+       (reduce +)))
 
 (defn parse-input
-  "Parses a comma-separated line of numbers from a string."
+  "Parses a comma-separated line of numbers from a string.
+  Returns a list of initial states for fish."
   [s]
   (->> (string/split s #",")
        (map string/trim)
        (map #(Integer/parseInt %))))
 
-;(defn mt-count-fish
-;  [filename days thread-count]
-;  (-> filename
-;      (slurp)
-;      (parse-input)
-;      (mt-simulation-run days thread-count)
-;      (count)))
-
-(defn simulation-run-analytical
-  [initial-state days]
-  '())
-
-(defn count-fish-analytical
-  [filename days]
-  (-> filename
-      (slurp)
-      (parse-input)
-      (simulation-run-analytical days)
-      (count)))
-
 (defn count-fish
+  "Counts the number of fish that would exist after a certain number of days.
+  The initial states for the fish are read from a file and are comma-separated."
   [filename days]
   (-> filename
       (slurp)
       (parse-input)
-      (simulation-run days)
-      (count)))
+      (simulation-run days)))
