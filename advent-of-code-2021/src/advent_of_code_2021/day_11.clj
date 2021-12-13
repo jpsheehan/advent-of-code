b86d17dd211b770d09ce128b29b81c1798d51f7c
Jesse Sheehan <jesse@sheehan.nz>
2021-12-13 13:46:13 +1300

    Working through day 11

--- advent-of-code-2021/src/advent_of_code_2021/day_11.clj
+++ advent-of-code-2021/src/advent_of_code_2021/day_11.clj
@@ -29,11 +29,78 @@
   [[_ flash-info]]
   (= flash-info :flash))
 
+(defn needs-to-flash?
+  [[energy-level flash-info]]
+  (and (= flash-info :no-flash) (> energy-level 9)))
+
 (defn reset-energy-levels
   [octopus]
   (if (has-flashed? octopus)
     [0 :no-flash]
-    [(first octopus) :no-flash]))
+    octopus))
+
+(defn get-octopus
+  [[x y] rows]
+  (nth (nth rows y) x))
+
+(defn find-flash-point
+  [rows]
+  (first (for [y (range (count rows))
+               x (range (count (first rows)))
+               :let [point [x y]]
+               :when (needs-to-flash? (get-octopus point rows))]
+           point)))
+
+(defn safely-increment-point
+  [target-point rows]
+  (let [height (count rows)
+        width (count (first rows))]
+    (partition width
+               (for [y (range height)
+                     x (range width)
+                     :let [point [x y]
+                           octopus (get-octopus point rows)]]
+                 (if (= target-point point)
+                   [(inc (first octopus)) (second octopus)]
+                   octopus)))))
+
+(defn apply-flash-point
+  [[x y] rows]
+  (->> rows
+       (safely-increment-point [x y])
+       (safely-increment-point [(inc x) y])
+       (safely-increment-point [(dec x) y])
+       (safely-increment-point [x (inc y)])
+       (safely-increment-point [x (dec y)])
+       (safely-increment-point [(inc x) (inc y)])
+       (safely-increment-point [(inc x) (dec y)])
+       (safely-increment-point [(dec x) (inc y)])
+       (safely-increment-point [(dec x) (dec y)])))
+
+(defn do-any-octopus-need-to-flash?
+  [rows]
+  (println "ADASD" rows)
+  (if (nil? (some needs-to-flash? (reduce concat rows)))
+    false
+    true))
+
+(defn compute-flashes
+  "Mark octopus that flash. Returns the new state."
+  [rows]
+  (loop [rows rows]
+    (if (not (do-any-octopus-need-to-flash? rows))
+      rows
+      (let [flash-point (find-flash-point rows)]
+        (if (nil? flash-point)
+          rows
+          (recur (apply-flash-point flash-point rows)))))))
+
+(defn reset-flashes
+  "Resets any octopus that flash. Returns the new state and the number of flashes that occurred."
+  [rows]
+  (let [num-flashes (count (filter has-flashed? (reduce concat rows)))
+        new-rows (map (fn [row] (map reset-energy-levels row)))]
+    (list num-flashes new-rows)))
 
 (defn step
   [rows]
@@ -42,19 +109,19 @@
        (map (fn [row] (map increase-energy-level row)))
 
        ; "Then, any octopus with an energy level greater than 9 flashes."
-       ; TODO
+       (compute-flashes)
 
        ; "Finally, any octopus that flashed during this step has its energy level set to 0"
-       (map (fn [row] (map reset-energy-levels row)))))
+       (reset-flashes)))
 
 (defn steps
-  [rows num-steps]
+  [num-steps rows]
   (loop [rows rows
          num-steps num-steps
          num-flashes 0]
     (if (zero? num-steps)
       num-flashes
-      (let [[rows num-flashes] (step rows)]
+      (let [[num-flashes rows] (step rows)]
         (recur rows (dec num-steps) num-flashes)))))
 
 (defn get-part-one
@@ -63,4 +130,5 @@
        (slurp)
        (parse-input)
        (decorate-input-with-flash-info)
+       (doall)
        (steps 100)))
--- advent-of-code-2021/test/advent_of_code_2021/day_11_test.clj
+++ advent-of-code-2021/test/advent_of_code_2021/day_11_test.clj
@@ -2,10 +2,59 @@
   (:require [clojure.test :refer :all]
             [advent-of-code-2021.day-11 :refer :all]))
 
+(def octopus-that-must-flash [10 :no-flash])
+(def octopus-that-has-flashed [11 :flash])
+(def octopus-that-has-reset [0 :no-flash])
+(def octopus-that-must-not-flash [9 :no-flash])
+
+(def octopus-rows-that-need-to-flash
+  [[octopus-that-has-reset octopus-that-must-not-flash]
+   [octopus-that-must-flash octopus-that-has-flashed]])
+
+(def octopus-rows-that-do-not-need-to-flash
+  [[octopus-that-has-reset octopus-that-must-not-flash]
+   [octopus-that-must-not-flash octopus-that-has-flashed]])
+
+(def octopus-rows-that-no-not-need-to-flash-increment-first
+  [[[1 :no-flash] octopus-that-must-not-flash]
+   [octopus-that-must-not-flash octopus-that-has-flashed]])
+
 (deftest parse-input-test
   (testing "Check that parsing input works"
     (is (= (parse-input "12345\n54321") '(( 1 2 3 4 5) (5 4 3 2 1))))))
 
+(deftest reset-energy-levels-test
+  (testing "Test an octopus that hasn't flashed"
+    (is (= (reset-energy-levels octopus-that-must-not-flash) octopus-that-must-not-flash)))
+  (testing "Test an octopus that has flashed"
+    (is (= (reset-energy-levels octopus-that-has-flashed) octopus-that-has-reset))))
+
+(deftest do-any-octopus-need-to-flash?-test
+  (testing "Test when no octopus need to flash"
+    (is (= (do-any-octopus-need-to-flash? octopus-rows-that-do-not-need-to-flash) false)))
+  (testing "Test when some octopus need to flash"
+    (is (= (do-any-octopus-need-to-flash? octopus-rows-that-need-to-flash) true))))
+
+(deftest find-flash-point-test
+  (testing "Test when no octopus need to flash"
+    (is (= (find-flash-point octopus-rows-that-do-not-need-to-flash) nil)))
+  (testing "Test when octopus need to flash"
+    (is (= (find-flash-point octopus-rows-that-need-to-flash) [0 1]))))
+
+(deftest needs-to-flash?-test
+  (testing "Test an octopus that needs to flash"
+    (is (= (needs-to-flash? octopus-that-must-flash) true)))
+  (testing "Test an octopus that has already flashed"
+    (is (= (needs-to-flash? octopus-that-has-flashed) false)))
+  (testing "Test an octopus that does not need to flash"
+    (is (= (needs-to-flash? octopus-that-must-not-flash) false))))
+
+(deftest safely-increment-point-test
+  (testing "Test incrementing an invalid point"
+    (= (safely-increment-point [2 4] octopus-rows-that-need-to-flash) octopus-rows-that-need-to-flash))
+  (testing "Test incrementing top-left point"
+    (= (safely-increment-point [0 0] octopus-rows-that-do-not-need-to-flash) octopus-rows-that-no-not-need-to-flash-increment-first)))
+
 (deftest get-part-one-test
   (testing "Check the example answer for part one"
-    (is (= (get-part-one "resources/day_11_input.txt") 1656))))
+    (is (= (get-part-one "resources/day_11_example.txt") 1656))))
